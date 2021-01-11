785a76d26c5bfe11c6088c5617a37e41cc441c17
Abbas Kagdi <24469915+AbbasKagdi@users.noreply.github.com>
2021-01-11 13:41:41 +0300

    Add files via upload

--- tot_graph.py
+++ tot_graph.py
@@ -9,6 +9,7 @@ def phi(n):
         return phi2(n)
 
 def phi1(n):
+    # odd
     result = 1
     for i in range(2, n):
         if (math.gcd(i, n) == 1):
@@ -16,6 +17,7 @@ def phi1(n):
     return result 
 
 def phi2(n):
+    # even
     result = 1
     # search on odd numbers only
     for i in range(3, n, 2):
@@ -23,7 +25,7 @@ def phi2(n):
             result+=1
     return result 
 
-x = np.arange(1,10001)
+x = np.arange(1,101)
 y = []
 for i in x:
     y.append(phi(i))
@@ -31,13 +33,16 @@ for i in x:
 #for i, j in zip(x, y):
     #print(i, "\t\t", j)
 
+def graph(formula, x_range):  
+    x2 = np.array(x_range)  
+    y2 = eval(formula)
+    plt.plot(x2, y2, label=formula)
 
-plt.plot(x, y) 
+graph('x-1', x)
 
-plt.xlabel("X") 
-
-plt.ylabel("φ(X)") 
-
-plt.title("Euler's Totient") 
-
+plt.plot(x, y, label="φ(X)", marker= ".", linewidth="0.005", color="#111")
+plt.xlabel("X")
+plt.ylabel("φ(X)")
+plt.legend() 
+plt.title("Euler's Totient")
 plt.show() 
