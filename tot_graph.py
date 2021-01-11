b815a74bdd9ab23849a6426f4241c3364d192c0d
Abbas Kagdi <24469915+AbbasKagdi@users.noreply.github.com>
2021-01-11 14:02:49 +0300

    Add files via upload
    1 to 1000

--- tot_graph.py
+++ tot_graph.py
@@ -3,35 +3,21 @@ import math
 import numpy as np
 
 def phi(n):
+    result, start, inc = 1, 3, 2
     if n%2 == 1:
-        return phi1(n)
-    else:
-        return phi2(n)
-
-def phi1(n):
-    # odd
-    result = 1
-    for i in range(2, n):
+        start, inc = 2, 1
+    for i in range(start, n, inc):
         if (math.gcd(i, n) == 1):
             result+=1
     return result 
 
-def phi2(n):
-    # even
-    result = 1
-    # search on odd numbers only
-    for i in range(3, n, 2):
-        if (math.gcd(i, n) == 1):
-            result+=1
-    return result 
-
-x = np.arange(1,101)
+x = np.arange(1,1001)
 y = []
 for i in x:
     y.append(phi(i))
 
-#for i, j in zip(x, y):
-    #print(i, "\t\t", j)
+for i, j in zip(x, y):
+    print(i, "\t\t", j)
 
 def graph(formula, x_range):  
     x2 = np.array(x_range)  
@@ -40,7 +26,8 @@ def graph(formula, x_range):
 
 graph('x-1', x)
 
-plt.plot(x, y, label="φ(X)", marker= ".", linewidth="0.005", color="#111")
+#plt.plot(x, y, label="φ(X)", marker= ".", linewidth="0.005", color="#111")
+plt.scatter(x, y, label="φ(X)", marker= ".", color="#111", s=1)
 plt.xlabel("X")
 plt.ylabel("φ(X)")
 plt.legend() 
